60a24809de43263e786fda2540461395edf0df38
Dallas Gale <gale.dallas@abc.net.au>
2022-08-19 13:34:05 +1000

    addede vercel config

--- next.config.js
+++ next.config.js
@@ -12,7 +12,11 @@ const nextConfig = {
           headers: [
             {
               key: "Cache-Control",
-              value: "public, max-age=0, must-revalidate",
+              value: "public, max-age=0, must-revalidate, Origin Control: Off",
+            },
+            {
+              key: "Origin-Control",
+              value: "Off",
             },
           ],
         },
