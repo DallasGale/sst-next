45c8638a51656e8ebc0de86fd43b8a375452fbc5
Dallas Gale <gale.dallas@abc.net.au>
2022-08-19 13:14:30 +1000

    headers

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const withVideos = require("next-videos");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: {
+    async headers() {
+      return [
+        {
+          source: "/",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=0, must-revalidate",
+            },
+          ],
+        },
+      ];
+    },
+  },
 };
 
 module.exports = withVideos(nextConfig);
