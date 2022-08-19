b3a72fed20055786d73cba7b3b5f906fe2bdd8ca
Dallas Gale <gale.dallas@abc.net.au>
2022-08-19 13:38:27 +1000

    addede vercel config

--- next.config.js
+++ next.config.js
@@ -4,16 +4,24 @@ const withVideos = require("next-videos");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  headers: [
-    {
-      key: "Cache-Control",
-      value: "public, max-age=0, must-revalidate, Origin Control: Off",
-    },
-    {
-      key: "Origin-Control",
-      value: "Off",
-    },
-  ],
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate, Origin Control: Off",
+          },
+          {
+            key: "Origin-Control",
+            value: "Off",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withVideos(nextConfig);
